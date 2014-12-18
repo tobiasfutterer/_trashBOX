43d6a56f7d908071685b9409ac0a313be54c766f
tobilalala <sade1337@web.de>
2014-12-18 15:09:20 +0100

    1st commit!

---
+++ trashbox/src/main/java/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("12MB");
+        factory.setMaxRequestSize("12MB");
+        return factory.createMultipartConfig();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
