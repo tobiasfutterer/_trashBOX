0945ffd8a2183c67162c7bc59f47b6ffb553a185
tobilalala <sade1337@web.de>
2015-01-06 21:22:33 +0100

    asd

--- trashbox/src/main/java/app/Application.java
+++ trashbox/src/main/java/app/Application.java
@@ -6,13 +6,6 @@ import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import javax.sql.DataSource;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import utils.ApplicationContextUtils;
 
 import javax.servlet.MultipartConfigElement;
 
@@ -28,12 +21,8 @@ public class Application {
 		factory.setMaxRequestSize("12MB");
 		return factory.createMultipartConfig();
 	}
-
-
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-
 }
